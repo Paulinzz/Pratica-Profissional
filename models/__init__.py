b60b4d543be8c0ca743dee407a18e4d7bb08a2fc
alanpereiradasilva <161897310+alanpereiradasilva@users.noreply.github.com>
2025-08-29 14:47:55 -0300

    autoincrementei

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     materias = db.relationship("Materia", backref="usuario", lazy=True)
@@ -14,13 +14,13 @@ class User(db.Model, UserMixin):
 
 class Materia(db.Model):
     __tablename__ = "tb_materias"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 
 class Atividade(db.Model):
     __tablename__ = "tb_atividades"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(200), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
