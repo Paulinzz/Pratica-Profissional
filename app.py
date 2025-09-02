3ec4cfe4f106f94594530870a2d825e61e859afb
alanpereiradasilva <161897310+alanpereiradasilva@users.noreply.github.com>
2025-09-02 15:31:25 -0300

    conectei e criei as tabelas

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 # Configuração para MySQL usando variáveis de ambiente
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME", "focosup")
+DB_NAME = os.getenv("DB_NAME", "pratica_profissional")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
